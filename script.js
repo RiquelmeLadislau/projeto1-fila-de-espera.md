0d787434dc84a9c7523622445c513340ff49a1ff
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-10-17 10:00:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const queue = [matheus,thierry,wellison]
+if(queue.length >= 0)
+let  = prompt(`Fila de pacientes
+
+    opções
+    1° Adicionar pacientes
+    2° Consultar paciente
+    3° Sair`)
+
+
